3dd4f7f0ba583a6d45f16f6a117ac0e89d99c03d
Naveen <adith.naveen@gmail.com>
2022-11-05 09:37:10 +0530

    condition checks

--- es6/callback-ex.js
+++ es6/callback-ex.js
@@ -1,11 +1,22 @@
 // synchronous 
 function divide(num1, num2) {
+
+    if (typeof num1 != 'number') {
+        throw ("Pls pass first arg as number");
+    }
+    if (typeof num2 != 'number') {
+        throw ("please pass second arg as number");
+    }
+    if (num1 === 0) {
+        throw ("Dont Divide by zero")
+    }
+
     return num1 / num2;
 }
 
 console.log("Start");
 let n1 = 100;
-let n2 = 40;
+let n2 = 0;
 
 let result = divide(n1, n2);
 console.log("Result is " + result);
