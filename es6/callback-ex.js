30ee33ca475ad530f82eaa4b58d5f217740bcf29
Naveen <adith.naveen@gmail.com>
2022-11-05 09:30:49 +0530

    working with callback

---
+++ es6/callback-ex.js
@@ -0,0 +1,14 @@
+// synchronous 
+function divide(num1, num2) {
+    return num1 / num2;
+}
+
+console.log("Start");
+let n1 = 100;
+let n2 = 40;
+
+let result = divide(n1, n2);
+console.log("Result is " + result);
+console.log("End");
+
+// your story will continue here also 
---
+++ readme.md
@@ -0,0 +1,11 @@
+# Day 2
+
+- synchronous call 
+```
+    function hi() {
+        // do something - 1s * x 
+    }
+    console.log("Start"); 
+        hi();
+    console.log("stop"); 
+```
